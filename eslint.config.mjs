6e38f8355a92fb50099fc2346fac320032dd3b2d
Christian Galsterer <christian.galsterer@gmx.de>
2026-09-19 08:04:15 +0200

    build: introduce eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+import love from 'eslint-config-love'
+import tseslint from 'typescript-eslint'
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+import importPlugin from 'eslint-plugin-import'
+
+export default tseslint.config(
+  love,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    name: 'base',
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error'
+    }
+  },
+  {
+    plugins: {
+      'import': importPlugin,
+      'simple-import-sort': simpleImportSortPlugin,
+      tseslint
+    },
+    rules: {
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/init-declarations': 'off',
+      complexity: 'off',
+      eqeqeq: ['error', 'always', { null: 'ignore' }],
+      'eslint-comments/require-description': 'off',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  },
+
+  {
+    ignores: ['dist/**/*.*', '**/*.js', '**/*.mjs']
+  },
+  {
+    name: 'src',
+    files: ['src/**/*.ts'],
+    ignores: ['test/**/*.ts']
+  },
+  {
+    name: 'test',
+    files: ['test/**/*.ts'],
+    rules: {
+      // you should turn the original rule off *only* for test files
+      '@typescript-eslint/no-magic-numbers': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      'eslint-comments/require-description': 'off'
+    }
+  }
+)
