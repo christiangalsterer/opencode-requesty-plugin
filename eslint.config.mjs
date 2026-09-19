812e7053e9cadccc84c2c97e6c5682e65f71e3c1
Christian Galsterer <christian.galsterer@gmx.de>
2026-09-19 10:15:02 +0200

    build: introduce husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,7 @@ export default tseslint.config(
   },
   {
     plugins: {
-      'import': importPlugin,
+      import: importPlugin,
       'simple-import-sort': simpleImportSortPlugin,
       tseslint
     },
